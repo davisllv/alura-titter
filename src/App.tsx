2d9f400522db140b507b83524b61f963549c4e5d
davisllv <davisllvsnt57@gmail.com>
2024-02-12 20:26:27 -0300

    configuring project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div className="bg-black">
+      <h1 className="text-3xl font-bold underline text-white">
+        Qualquer coisa!
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-20 bg-blue">
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
