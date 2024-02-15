c8de87bf71782d2d38c33fe98f6b2ff35f57ea9a
davisllv <davisllvsnt57@gmail.com>
2024-02-14 23:10:05 -0300

    initiate the error page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,27 @@ const NotFound = () => {
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div
+      className="w-full h-full items-center flex justify-center flex-col gap-10 bg-zinc-400 font-fira-sans"
+      id="error-page"
+    >
+      <div>
+        <span className="text-10xl">4</span>
+        <span>{"</>"}</span>
+        <span>4</span>
+      </div>
+
+      <code>
+        <span>Error404</span>
+        <span className="text-white">{`( ) {`}</span>
+        <br />
+        <span className="ml-10">
+          message <b>= </b>
+        </span>
+        <span>{"Page not found"}</span>
+        <br />
+        <span className="text-white">{`}`}</span>
+      </code>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
